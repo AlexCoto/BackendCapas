1cbb90e7cb6fa4861b1599febc9b9a8d46b5a168
AlexCoto <68604189+AlexCoto@users.noreply.github.com>
2022-02-19 20:55:24 -0600

    Initial commit

---
+++ Backend/BE.API/Controllers/Categories.cs
@@ -0,0 +1,22 @@
+﻿using BE.DAL.EF;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE.API.Controllers
+{
+    public class Categories : Controller
+    {
+ 
+        private readonly NDbContext _context;
+
+        public CategoriesController(NDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categories>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
